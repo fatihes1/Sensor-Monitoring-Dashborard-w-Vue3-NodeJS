a8356d73a311d0679f035814b5ce7037c65bd9ba
Fatih Es <54971670+fatihes1@users.noreply.github.com>
2022-06-05 15:18:49 +0300

    created sensors page will add detail sensor

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import WelcomePage from "@/components/WelcomePage.vue";
 import HomePage from "@/components/HomePage.vue"
+import AllSensors from "@/components/AllSensors.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,12 @@ const router = createRouter({
       name : "home",
       path : "/home",
       component: HomePage
+    },
+    {
+      name : "mainpoint",
+      path : "/mainpoint/:id",
+      component : AllSensors,
+      props: true
     }
   ],
 });
