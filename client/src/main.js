c5fbf73852f35e3177d841f9df495f4c07e1febe
Fatih Es <54971670+fatihes1@users.noreply.github.com>
2022-06-09 22:31:56 +0300

    added toast notification

--- client/src/main.js
+++ client/src/main.js
@@ -10,11 +10,18 @@ import { appAxios } from "./utils/appAxios";
 import io from "socket.io-client";
 const socket = io("http://localhost:8080");
 
+import Toast from "vue-toastification";
+// Import the CSS or use your own!
+import "vue-toastification/dist/index.css";
+
 const app = createApp(App);
 
 
 app.use(createPinia());
 app.use(router);
+app.use(Toast);
+
+
 
 app.config.globalProperties.$socket = socket;
 app.config.globalProperties.$appAxios = appAxios;
